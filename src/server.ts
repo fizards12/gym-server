9550a3bd6913450ead575b8b72ec3ff9f5ed1c50
fizards12 <mahmoudsameh734@gmail.com>
2024-05-06 23:29:06 +0300

    server.js changed

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,29 @@ import express, { Application,Request,Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv"
+import { Result, Schema, checkSchema, query, validationResult } from "express-validator";
+import { ResultWithContext } from "express-validator/src/chain";
 
 dotenv.config();
 const port: number = (process.env.PORT as unknown) as number;
 const app: Application = express();
-
+const schema : Schema = {
+    name: {
+        trim: true,
+        notEmpty: {
+            errorMessage: "Enter valid name"
+        },
+    }
+}
 app.use(cors({ origin: "*" }))
 app.use(express.json());
 
 mongoose.set("strictQuery", false);
-app.get("/",(req:Request,res:Response)=>{
-    res.send("<h1>Hello, World!</h1>")
+app.get("/:name",async(req:Request,res:Response)=>{
+
+    const result : ResultWithContext[] = await checkSchema(schema).run(req);
+
+    res.send(result)
 })
 
 app.listen(port, () => {
