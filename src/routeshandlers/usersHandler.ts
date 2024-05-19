edcec72ac071c16ec6fecae318882e4178830da2
your_name <your_email_id>
2024-05-19 22:29:13 +0200

    deploying application

--- src/routeshandlers/usersHandler.ts
+++ src/routeshandlers/usersHandler.ts
@@ -14,7 +14,7 @@ const saltRounds: number = +(process.env.SALTS_ROUNDS as unknown as number);
 
 export const getAll = async (req: CustomRequest, res: Response, next: NextFunction): Promise<Response | undefined> => {
     try {
-        const users = await User.find({});
+        const users = await User.find({}).populate("notifications");
         if (req.accJWE) {
             return res.status(200).json({ users, token: req.accJWE });
         }
@@ -36,7 +36,7 @@ export const getUser = async (req: CustomRequest, res: Response, next: NextFunct
             }
         }
         const id = req.params.id
-        const user = await User.findOne({ member_id: id });
+        const user = await User.findOne({ member_id: id }).populate("notifications");
         if (!user) {
             throw {
                 name: Errors.CREDENTIALS_ERROR,
